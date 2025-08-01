0ebbb662364e017054a746b20ecaee1b4cd238bd
BuddoluAnoopKumar <135159262+BuddoluAnoopKumar@users.noreply.github.com>
2025-08-01 12:54:05 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"  
+}
+
+resource "aws_instance" "foo" {
+  ami           = "ami-08a6efd148b1f7504" # us-west-2
+  instance_type = "t2.micro"
+  tags = {
+      Name = "TF-Instance"
+  }
+}
